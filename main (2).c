d314c4328abd1f862c94d6643a5f253faa7edc93
alisha0710 <alishahoney0710@gmail.com>
2026-09-13 12:33:40 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int i;
+ for(i=1;i<=20;i++){
+     if(i%2==0){
+         printf("%d\n" ,i);
+         
+     }
+ }
+
+    return 0;
+}
