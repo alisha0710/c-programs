3585e1a2a872467f1a13a8ea94566f18bab81dd6
alisha0710 <alishahoney0710@gmail.com>
2026-09-13 23:01:36 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int i;
+for(i=1,i<=20;i++){
+    if(i==15){
+        break;
+        
+    }
+    printf("%d\n" ,i)
+}
+    return 0;
+}
