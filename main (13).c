98784b5389143429e89af72a075b50fbe4b00964
alisha0710 <alishahoney0710@gmail.com>
2026-09-14 09:56:26 +0530

    Add files via upload

---
+++ main (13).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=1;i<=50;i++){
+        if(i%2==0){
+            continue;
+        }
+        printf("%d\n" ,i);
+    }
+    return 0;
+}
